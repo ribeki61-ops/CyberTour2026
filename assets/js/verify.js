4dcf64972cb50f3f18c0fc632d2557b160411644
ribeki61-ops <ribeki61@gmail.com>
2026-09-18 00:39:05 +0200

    Update verify.js

--- assets/js/verify.js
+++ assets/js/verify.js
@@ -54,7 +54,7 @@ async function startSelfie(onSuccess) {
   video.srcObject = stream;
   try { await video.play(); } catch {}
 
-  setTimeout(() => captureSelfie(video, onSuccess), 150);
+  setTimeout(() => captureSelfie(video, onSuccess), 250);
 }
 
 function captureSelfie(video, onSuccess) {
